5bb1b6b1c123e4d2f1f91f92fe431273c6129309
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2024-06-13 12:53:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,7 +68,7 @@ GIT_TOKEN = getenv(
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/AF_vibe_support")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/gitwizardbypass")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/AF_Support_Group")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
